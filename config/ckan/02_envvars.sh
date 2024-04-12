343f609c012b7ef587d1567a75b3331760b400f4
Thomas Hanke <thomas.hanke@iwm.fraunhofer.de>
2024-04-12 08:18:12 +0200

    speedup for env parsing to ckan.ini

--- config/ckan/02_envvars.sh
+++ config/ckan/02_envvars.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+temp_ini="options.ini"
+
 function configure_ckan_datapusher {
+  touch $temp_ini
   for var in $(env | grep "^CKANINI" | cut -d= -f1); do
     variable_name=$(echo "${var#CKANINI__}" | tr '[:upper:]' '[:lower:]' | sed 's/__/\./g')
     value=$(eval "echo \$$var")
-    ckan config-tool $CKAN_INI "${variable_name}=${value}"
-    echo "Set ${variable_name} in ${CKAN_INI}"
+    echo "$var -> ${variable_name}=${value}"
+    echo "${variable_name}=${value}" >> $temp_ini
   done
+  ckan config-tool $CKAN_INI -f $temp_ini
+  rm $temp_ini
 }
 
 configure_ckan_datapusher
